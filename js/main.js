5df6385bf66c5506ef235a27111d8d56c728e2e6
Enric P <kikflip@gmail.com>
2014-06-17 18:01:40 +0200

    notificacio

--- js/main.js
+++ js/main.js
@@ -14,9 +14,20 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new WebSqlStore();
+		var self = this;
+        this.store = new WebSqlStore(function() {
+		  self.showAlert('Store Initialized', 'info');
+		});
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    },
+	
+	showAlert: function (message, title) {
+	  if (navigator.notification) {
+	    navigator.notification.alert(message, null, title, 'OK');
+	  } else {
+		  alert(title ? (title + ": " + message) : message);
+	  }
+	}
 
 };
 
